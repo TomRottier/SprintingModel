db1a80f948c366ea08748d2fce392a6b64acc5a9
TomRottier <39778698+TomRottier@users.noreply.github.com>
2020-10-12 22:25:12 +0100

    Tidy up code

--- Model/ConservationChecks/conservationPlot.m
+++ Model/ConservationChecks/conservationPlot.m
@@ -3,19 +3,27 @@ clear; close all; clc;
 file1 = importdata('sprintmodel.5', ' ', 8);
 file2 = importdata('sprintmodel.1', ' ', 8);
 data = file1.data;
-vcmx = file2.data(:,end-1);
 time = data(:,1);
 
+dE = data(end,4) - data(1,4);
+dH = data(end,5) - data(1,5);
+dP = (data(end,6)+data(end,7)) - (data(1,6)+data(1,7));
+
 %% Plot
-set(figure(1),'WindowStyle','docked')
-subplot(2,1,1); cla; hold on
+set(figure(1),'WindowStyle','docked'); cla; hold on
 plot(time, data(:,[2 3 4]))
 legend('KECM','PECM','TE', 'location','bestoutside')
-title('Energy')
+title(['\Delta energy: ' num2str(dE)])
 
-subplot(2,1,2); cla; hold on
-plot(time, data(:,end))
-title('Angular momentum')
 
 set(figure(2),'WindowStyle','docked')
-plot(time, vcmx)
+subplot(2,1,1); hold on; cla;
+plot(time, data(:,5))
+title(['\Delta angular momentum: ' num2str(dH)])
+legend('z','location','bestoutside')
+
+subplot(2,1,2); hold on; cla
+plot(time, data(:,[6 7]))
+title(['\Delta linear momentum: ' num2str(dP)])
+legend('x','y','location','bestoutside')
+
