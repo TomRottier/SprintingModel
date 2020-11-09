8715acab8c942307ada9a3cd1ea253107cf77d41
TomRottier <39778698+TomRottier@users.noreply.github.com>
2020-11-09 18:26:56 +0000

    Conservation checks

---
+++ Model/7seg/ConservationChecks/conservationPlots.m
@@ -0,0 +1,46 @@
+clear; close all; clc
+% Show that model conserves energy (when no energy added) and angular
+% momentum (when no external forces).
+%
+%% Import data
+file1 = importdata('7segSprint.5', ' ', 8);
+file2 = importdata('7segSprint.1', ' ', 8);
+data = file1.data;
+time = data(:,1);
+
+dE = data(end,4) - data(1,4);
+dH = data(end,5) - data(1,5);
+dP = (data(end,6)+data(end,7)) - (data(1,6)+data(1,7));
+
+%% Plot
+set(figure(1),'WindowStyle','docked'); cla; hold on
+plot(time, data(:,[2 3 4]))
+legend('KECM','PECM','TE', 'location','bestoutside')
+title(['\Delta energy: ' num2str(dE)])
+
+
+set(figure(2),'WindowStyle','docked')
+subplot(2,1,1); hold on; cla;
+plot(time, data(:,5))
+title(['\Delta angular momentum: ' num2str(dH)])
+legend('z','location','bestoutside')
+ylim([-40 40])
+
+subplot(2,1,2); hold on; cla
+plot(time, data(:,[6 7]))
+title(['\Delta linear momentum: ' num2str(dP)])
+legend('x','y','location','bestoutside')
+
+%% Draw model
+% data = file2.data; n = length(data);
+% set(figure(3),'WindowStyle','docked'); cla; hold on
+% xlim([-1 1.5]); ylim([-0.1 2.4]);
+% for i = 1:1:n
+%     cla
+%     line(data(i,2:2:12), data(i,3:2:13))
+%     line(data(i,[8 14]), data(i,[9 15]))
+%     plot(data(i,end-3), data(i,end-2), 'kx')
+%     drawnow 
+%     pause(0.01)
+% end
+
---
+++ Model/7seg/ConservationChecks/notes.md
@@ -0,0 +1,15 @@
+# Energy and momentum conservation
+
+Tests to determine if the equations of model for the model are valid and that energy and momenta are conserved in appropiate situations.
+
+## Energy
+
+By setting torque generator activations, specified mass and angle functions and spring damping parameters set to zero, no energy is gained or lost from the model.
+
+![image info](energy.png)
+
+## Momentum
+
+With spring forces and gravity set to zero, no external forces or torques are acting on the system so both linear and angular momentum should be conserved.
+
+![image info](momentum.png)
